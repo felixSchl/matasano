75de9ed53eb4487371cef4f0a260f68dfa60d9bb
Felix Schlitter <felixschlitter@gmail.com>
2016-01-21 14:37:54 +1300

    Add test

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,13 @@ fn hex_to_base64(str: &str) -> String {
         .to_base64(STANDARD);
 }
 
+#[test]
+fn challenge_1() {
+    assert_eq!(
+        hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"),
+        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    )
+}
+
 fn main() {
-    println!("{}", hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
 }
