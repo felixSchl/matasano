afa12d67ec4a026a5aee5d5059e810884e8be2af
Felix Schlitter <felixschlitter@gmail.com>
2016-01-25 13:59:16 +1300

    Solve challenge 4 of set 1

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::fs::File;
 use std::cmp;
 use std::cmp::Ordering;
 use std::ascii::{ AsciiExt };
-use std::io::{ self, BufReader };
+use std::io::{ BufReader };
 use std::io::prelude::*;
 use serialize::base64::{ ToBase64, STANDARD };
 use serialize::hex::{ FromHex, ToHex };
@@ -54,15 +54,19 @@ macro_rules! hashmap {
 
 #[derive(Debug)]
 struct Ngram {
-    L: u32,
-    N: u32,
+    size: u32,
+    total: u32,
     floor: f32,
     table: HashMap<String, f32>,
 }
 
+/// N-gram based crypto frequency analysis based on the
+/// "practicalcryptography.com" articles on the topic.
+///
+/// Reference implementation:
+/// http://practicalcryptography.com/media/cryptanalysis/files/ngram_score_1.py
 impl Ngram {
     fn score(&self, input: &str) -> f32 {
-
         let mut score = 0f32;
 
         let plaintext = {
@@ -71,10 +75,10 @@ impl Ngram {
         };
 
         for word in plaintext.split(" ") {
-            for i in 0..cmp::max(0, (word.len() as i32) - (self.L as i32)) {
+            for i in 0..cmp::max(0, (word.len() as i32) - (self.size as i32)) {
                 let gram = word.chars()
                     .skip(i as usize)
-                    .take(self.L as usize)
+                    .take(self.size as usize)
                     .collect::<String>();
                 score += *self.table
                     .get(&gram)
@@ -84,86 +88,78 @@ impl Ngram {
 
         return score
     }
-}
 
-fn load_training_set(path: &str) -> Ngram {
-    let f = File::open(path).unwrap() /* XXX */;
-    let f = BufReader::new(f);
+    fn from_file(path: &str) -> Ngram {
+        let f = File::open(path).unwrap() /* XXX */;
+        let f = BufReader::new(f);
 
-    // populate the count-table
-    let mut sum = 0;
-    let mut table = HashMap::<String, f32>::new();
-    let mut keylen = None;
-    for line in f.lines() {
-        let line = line.unwrap() /* XXX */;
-        let xs = line.split(' ').collect::<Vec<_>>();
-        if let Some((key, val)) = xs.get(0).and_then(|key| {
-            let key = key.to_string();
-            xs.get(1)
-                .and_then(|val| val.parse::<u32>().ok())
-                .map(|val| (key, val))
-        }) {
+        // populate the count-table
+        let mut sum = 0;
+        let mut table = HashMap::<String, f32>::new();
+        let mut keylen = None;
+        for line in f.lines() {
+            let line = line.unwrap() /* XXX */;
+            let xs = line.split(' ').collect::<Vec<_>>();
+            if let Some((key, val)) = xs.get(0).and_then(|key| {
+                let key = key.to_string();
+                xs.get(1)
+                    .and_then(|val| val.parse::<u32>().ok())
+                    .map(|val| (key, val))
+            }) {
 
-            let new_keylen = match keylen {
-                None         => Ok(key.len() as u32),
-                Some(keylen) => {
-                    if keylen == key.len() as u32 {
-                        Ok(keylen as u32)
-                    } else {
-                        Err("key-lengths are not homogenous")
+                let new_keylen = match keylen {
+                    None         => Ok(key.len() as u32),
+                    Some(keylen) => {
+                        if keylen == key.len() as u32 {
+                            Ok(keylen as u32)
+                        } else {
+                            Err("key-lengths are not homogenous")
+                        }
                     }
-                }
-            };
+                };
 
-            sum += val;
-            keylen = Some(new_keylen.unwrap() /* XXX */);
-            table.insert(key, (val as f32));
+                sum += val;
+                keylen = Some(new_keylen.unwrap() /* XXX */);
+                table.insert(key, (val as f32));
+            }
+        }
+
+        // convert the counts to probabilities
+        for (_, val) in table.iter_mut() {
+            *val = (*val / (sum as f32)).log10();
+        }
+
+        // calculate the base value
+        let floor = (0.01f32 / (sum as f32)).log10();
+
+        return Ngram {
+            size:  keylen.unwrap() /* XXX */,
+            total: sum,
+            floor: floor,
+            table: table,
         }
     }
-
-    // convert the counts to probabilities
-    for (_, val) in table.iter_mut() {
-        *val = (*val / (sum as f32)).log10();
-    }
-
-    // calculate the base value
-    let floor = (0.01f32 / (sum as f32)).log10();
-
-    return Ngram {
-        L:     keylen.unwrap() /* XXX */,
-        N:     sum,
-        floor: floor,
-        table: table,
-    }
 }
 
-fn calc_score(input: &str) -> f32 {
-    let ngram = load_training_set("./resources/english_trigrams.txt");
-    ngram.score(&input)
-}
+fn single_byte_xor(bytes: Vec<u8>, ngram: &Ngram) -> Option<Vec<String>> {
 
-fn single_byte_xor(bytes: Vec<u8>) -> Option<Vec<String>> {
-    let chars=32u8..128u8;
-    let mut results = chars.clone()
-        .filter_map(|c| {
+    let mut chars = (0u8..255u8).collect::<Vec<u8>>();
+    chars.push(255u8); // ... because the range end is exclusive, and `0..256` does not work.
+    let mut results = chars.iter()
+        .map(|c| {
             bytes.iter()
-                .fold(Some(vec![]), |acc, x| acc.and_then(|mut data| {
-                    let xor = x ^ c;
-                    if xor >= 32u8 /* skip control characters */ {
-                        data.push(xor);
-                        Some(data)
-                    } else {
-                        None
-                    }
-                }))
+                .fold(vec![], |mut acc, x| {
+                    acc.push((x ^ c) as char);
+                    acc
+                })
+                .into_iter()
+                .collect::<String>()
         })
-        .map(|b| String::from_utf8(b).unwrap())
-        .map(|s| (s.clone(), calc_score(&s)))
+        .map(|s| (s.clone(), ngram.score(&s)))
         .collect::<Vec<_>>();
 
-    // XXX: No compare instance for f32?
-    results.sort_by(|&(_, scoreA), &(_, scoreB)| {
-        scoreB.partial_cmp(&scoreA).unwrap_or(Ordering::Equal)
+    results.sort_by(|&(_, score_a), &(_, score_b)| {
+        score_b.partial_cmp(&score_a).unwrap_or(Ordering::Equal)
     });
 
     Some(
@@ -176,41 +172,46 @@ fn single_byte_xor(bytes: Vec<u8>) -> Option<Vec<String>> {
 #[test]
 fn challenge_3() {
     let input="1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+
+    let ngram = Ngram::from_file("./resources/english_trigrams.txt");
+
     assert_eq!(
         input.from_hex().ok()
-            .and_then(single_byte_xor).unwrap()
+            .and_then(|x| single_byte_xor(x, &ngram)).unwrap()
             .first().unwrap(),
         "Cooking MC's like a pound of bacon"
     );
 }
 
-// #[test]
-// fn challenge_4() {
-//     let f = File::open("./fixtures/set1_challenge4.txt").unwrap();
-//     let f = BufReader::new(f);
-//
-//     let mut results = f.lines()
-//         .map(|l| l.unwrap())
-//         .filter(|l| l.len() == 60)
-//         .filter_map(|l| l.from_hex().ok())
-//         .filter_map(|l| single_byte_xor(l))
-//         .filter_map(|xs| xs.first().map(|x| x.clone()))
-//         .map(|str| {
-//             let bytes = str.chars();
-//             (
-//                 str.clone(),
-//                 bytes.fold(0, |acc, x| acc + calc_score(x))
-//             )
-//         })
-//         .collect::<Vec<_>>();
-//     results.sort_by(|&(_, scoreA), &(_, scoreB)| scoreB.cmp(&scoreA));
-//     let results = results.iter()
-//         .map(|&(ref x, _)| x.clone())
-//         .collect::<Vec<String>>();
-//
-//     println!("{}", results.first().unwrap());
-// }
+#[test]
+fn challenge_4() {
+    let ngram = Ngram::from_file("./resources/english_trigrams.txt");
 
+    let f = File::open("./fixtures/set1_challenge4.txt").unwrap();
+    let f = BufReader::new(f);
+
+    // select the best single-byte-xor for each input
+    let mut candidates = f.lines()
+        .filter_map(|l| l.ok())
+        .filter(|l| l.len() == 60)
+        .filter_map(|l| l.from_hex().ok())
+        .filter_map(|l| single_byte_xor(l, &ngram))
+        .filter_map(|xs| xs.first().map(|x| x.clone()))
+        .map(|ref x| (x.clone(), ngram.score(&x)))
+        .collect::<Vec<_>>();
+
+    // sort each candidate by it's english-ness
+    candidates.sort_by(|&(_, score_a), &(_, score_b)| {
+        score_b.partial_cmp(&score_a).unwrap_or(Ordering::Equal)
+    });
+
+    // select the winner
+    let results = candidates.iter()
+        .map(|&(ref x, _)| x.clone())
+        .collect::<Vec<String>>();
+
+    assert_eq!(results.first().unwrap(), "Now that the party is jumping\n");
+}
 
 fn main() {
 }
